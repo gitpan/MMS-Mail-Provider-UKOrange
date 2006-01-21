5060cc367d449f581ec3c8c532e12d44759d7ab9
Rob Lee <robl@cpan.org>
2006-01-21 13:17:05 +0000

    Import of ROBL/MMS-Mail-Provider-UKOrange-0.05 from CPAN.
    gitpan-cpan-distribution: MMS-Mail-Provider-UKOrange
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         ROBL/MMS-Mail-Provider-UKOrange-0.05.tar.gz
    gitpan-cpan-author:       ROBL
    gitpan-cpan-maturity:     released

--- lib/MMS/Mail/Provider/UKOrange.pm
+++ lib/MMS/Mail/Provider/UKOrange.pm
@@ -13,11 +13,11 @@ MMS::Mail::Provider::UKOrange - This provides a class for parsing an MMS::Mail::
 
 =head1 VERSION
 
-Version 0.04
+Version 0.05
 
 =cut
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 =head1 SYNOPSIS
 
